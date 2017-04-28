579bc88fad10a725ea172c79d4cc12a671c80546
felipebs <felipebahiensesantos@gmail.com>
2017-04-28 01:09:20 -0300

    ajuste no logout

--- php/menu.php
+++ php/menu.php
@@ -1,6 +1,6 @@
 <nav>
   <ul>
     <li><a href="../solicitacao_form.php">Solicitar Serviço</a></li>
-    <li><a href="php/logout.php">Logout</a></li>
+    <li><a href="logout.php">Logout</a></li>
   </ul>
 </nav>
