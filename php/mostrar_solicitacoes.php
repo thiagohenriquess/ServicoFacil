e9c19585651e98bcb73ec6e79a2e857fd65c1c55
Thiago Henrique <thiagohenriq@gmail.com>
2017-04-28 01:57:18 -0300

    Adicionando servico no painel e melhorando tratamento

--- php/mostrar_solicitacoes.php
+++ php/mostrar_solicitacoes.php
@@ -20,43 +20,62 @@
 
   $table = 'solicitacao';
   $params = "WHERE usuario_id = ".$usuario_id;
-  $fields = "numero, tipo_servico_id,status, data_inicio";
+  $fields = "numero, tipo_servico_id, servico_id,status, data_inicio";
   $verifica = DBRead($table, $params, $fields);
 
 
   if($verifica){
     echo "<table>
       <tr>
-        <th>numero</th>
-        <th>tipo_servico_id</th>
-        <th>status</th>
-        <th>data_inicio</th>
+        <th>Número</th>
+        <th>Tipo Serviço</th>
+        <th>Serviço</th>
+        <th>Status</th>
+        <th>Data Início</th>
       </tr>"
     ;
     foreach ($verifica as $key => $array) {
       echo "<tr>";
 
-      foreach ($array as $key2 => $value) {
-        if ($key2 = 'tipo_servico_id') {
-          $tableTipoServico = 'tipo_servico';
-          $paramsTipoServico = "WHERE tipo_servico_id = '$value'";
-          $fieldsTipoServico = "nome";
-          $tipo_servico = DBRead($tableTipoServico, $paramsTipoServico, $fieldsTipoServico);
-          if($tipo_servico){
-            foreach ($tipo_servico as $key3 => $array2) {
-              foreach ($array2 as $key4 => $value2) {
-                $value = $value2;
-              }
-            }
-        }
-      }
+        $numero = $array['numero'];
+        $tipo_servico_id = $array['tipo_servico_id'];
+        $servico_id = $array['servico_id'];
+        $status = $array['status'];
+        $data = $array['data_inicio'];
 
-        echo "<td>$value</td>";
-      }
+        $tableTipoServico = 'tipo_servico';
+        $paramsTipoServico = "WHERE tipo_servico_id = '$tipo_servico_id'";
+        $fieldsTipoServico = "nome";
+        $tipo_servico = DBRead($tableTipoServico, $paramsTipoServico, $fieldsTipoServico);
+
+        if($tipo_servico){
+          foreach ($tipo_servico as $key3 => $array2) {
+              foreach ($array2 as $key4 => $value2) {
+                $tipo_servico_nome = $value2;
+              }
+          }
+        }
+
+        $tableServico = 'servico';
+        $paramsServico = "WHERE servico_id = '$servico_id'";
+        $fieldsServico = "nome";
+        $servico = DBRead($tableServico, $paramsServico, $fieldsServico);
+
+        if($servico){
+          foreach ($servico as $key5 => $array3) {
+            foreach ($array3 as $key6 => $value3) {
+              $servico_nome = $value3;
+            }
+          }
+        }
+        echo "<td>$numero</td>"  ;
+        echo "<td>$tipo_servico_nome</td>";
+        echo "<td>$servico_nome</td>";
+        echo "<td>$status</td>";
+        echo "<td>$data</ td>";
       echo "</tr>";
     }
     echo "</table>";
-
 
   }
 ?>
